19623abb1228e0ba3668352d29b3a0652731c2cd
omar <omar.masood@ziyotek.net>
2026-02-02 15:42:54 -0500

    update files

---
+++ terraform/outputs.tf
@@ -0,0 +1,9 @@
+output "public_ip" {
+  description = "Public IP of the web server"
+  value       = aws_instance.web.public_ip
+}
+
+output "public_dns" {
+  description = "Public DNS of the web server"
+  value       = aws_instance.web.public_dns
+}
